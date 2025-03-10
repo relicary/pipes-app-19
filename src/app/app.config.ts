50a270a410b7d9f82221082cee2f10db7d7c9b6e
Juan Cremades <relicary@gmail.com>
2025-03-10 15:43:26 +0100

    i18n: Add languages to App Config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+
+import localEs from '@angular/common/locales/es'
+import localDe from '@angular/common/locales/de'
+
+registerLocaleData(localEs, 'es');
+registerLocaleData(localDe, 'de')
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'de'
+    }
+  ]
 };
