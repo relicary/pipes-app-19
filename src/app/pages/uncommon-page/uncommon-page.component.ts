3e694ac778718ca6eace71f8891a6e4f9f1516dc
Juan Cremades <relicary@gmail.com>
2025-03-10 17:35:07 +0100

    PIPE: Use I18nSelectPipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
+import { I18nSelectPipe } from '@angular/common';
+
+const customer1 = {
+  name: 'Relicary',
+  gender: 'male',
+  age: 42,
+  address: 'Valencia, Spain'
+};
+
+const customer2 = {
+  name: 'Neimhain',
+  gender: 'female',
+  age: 36,
+  address: 'Sevilla, Spain'
+};
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+
+  // i18n Select
+  customer = signal(customer1);
+
+  invitationMap = {
+    male: 'mister',
+    female: 'miss',
+  }
+
+  changeCustomer() {
+    if (this.customer() === customer1) {
+      this.customer.set(customer2);
+      return;
+    }
+
+    this.customer.set(customer1);
+
+  }
+}
