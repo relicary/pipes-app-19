dbb32da2cd38449766162a77c203b3de6cf33f6f
Juan Cremades <relicary@gmail.com>
2025-03-11 17:34:57 +0100

    PIPE: Chained pipes

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -4,10 +4,11 @@ import { heroes } from '../../data/hero.data';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 import { HeroColorPipe } from '../../pipes/hero-color.pipe';
 import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
