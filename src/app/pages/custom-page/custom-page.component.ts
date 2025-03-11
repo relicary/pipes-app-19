1e2f2b4ceaa38d6529fd3d6708c925e8c51bcbdb
Juan Cremades <relicary@gmail.com>
2025-03-11 17:59:18 +0100

    PIPE: Added a HeroSortBy pipe

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -6,10 +6,12 @@ import { HeroColorPipe } from '../../pipes/hero-color.pipe';
 import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
 import { TitleCasePipe } from '@angular/common';
 import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe, HeroSortByPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
@@ -18,5 +20,7 @@ export default class CustomPageComponent {
   upperCase = signal(true);
 
   heroes = signal(heroes);
+
+  sortBy = signal<keyof Hero | null>(null);
 
 }
