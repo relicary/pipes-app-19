c66e7f273c2cae581a6efc27caeb9b4d1768e3f3
Juan Cremades <relicary@gmail.com>
2025-03-11 13:31:29 +0100

    PIPE: Added ToggleCase pipe, a custom pipe

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [ToggleCasePipe],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent { }
+export default class CustomPageComponent {
+  name = signal('Relicary Vlc')
+}
